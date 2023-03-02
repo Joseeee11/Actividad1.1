dfcd422953db3b56b767d443d618f0b139e26bda
SamTempest <118561364+SamTempest@users.noreply.github.com>
2023-03-02 08:11:44 -0400

    Update personal.c.js

--- controllers/personal.c.js
+++ controllers/personal.c.js
@@ -9,6 +9,28 @@ class personalControllers {
     return databased.database.personal;
     };
 
+    //POST crear un personal dentro de la BD, con un identificador (CI) único
+    crear(personalc){
+
+        //verificar que contenga todas las determinadas propiedades
+        if (!personalc.Nombre_apellido || !personalc.CI || !personalc.nombre_usuario_unico || !personalc.contraseña || !personalc.especialidad || !personalc.cargo) {
+
+            return ("No se agregó el personal, se requiere del personal: Nombre y Apellido, CI, Usuario, Contraseña, Cargo, Especialidad");
+
+        }else{
+
+        //recorrer cada personal para verificar que la CI no se repita, es decir sea un identificador único
+            for (let i = 0; i < databased.database.personal.length; i++) {
+                if (personalc.CI === databased.database.personal[i].CI){
+                return (`No se agrego porque el personal con la C.I: ${personalc.CI}  ya existe`);
+                };
+            };
+
+            console.log('se agregó'); 
+            databased.database.personal.push(personalc); //se agrega a la BD
+            return ("PERSONAL AGREGADO EXITOSAMENTE");
+        };
+    };
 
 };
 
